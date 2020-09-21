7d287184b28a977cc4f0890fcad3575e6cfc69ed
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-09-21 11:29:23 -0400

    Part 03
    Updated Path to work on multiple platforms (widows or mac)

--- scenefile.py
+++ scenefile.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 class SceneFile(object):
     """An abstract representation of a Scene file."""
     def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = folder_path
+        self.folder_path = Path(folder_path)
         self.descriptor = descriptor
         self.task = task
         self.ver = ver
@@ -12,5 +14,9 @@ class SceneFile(object):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
         return pattern.format(descriptor=self.descriptor, task=self.task, ver=self.ver, ext=self.ext)
 
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+
 scene_file = SceneFile("D:\\", "tank", "model", 1, ".ma")
-print(scene_file.filename)
+print(scene_file.path)
