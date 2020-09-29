939d5220511545040c3cae1ebd5995bdab0e05f6
Jaelen Hawthorne <jeh160230@utdallas.edu>
2020-09-29 17:49:49 -0400

    Part 2 of Assignment 03
    Added Save() function

--- scenefile.py
+++ scenefile.py
@@ -1,4 +1,9 @@
+import logging
+
+import pymel.core as pmc
 from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
 
 
 class SceneFile(object):
@@ -14,7 +19,10 @@ class SceneFile(object):
     @property
     def filename(self):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
-        return pattern.format(descriptor=self.descriptor, task=self.task, ver=self.ver, ext=self.ext)
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
 
     @property
     def path(self):
@@ -27,7 +35,15 @@ class SceneFile(object):
         self.descriptor, self.task, ver = path.name.stripext().split("_")
         self.ver = int(ver.split("v")[-1])
 
+    def save(self):
+        """ Saves the scene file.
 
-scene_file = SceneFile("D:/tank_model_v001.ma")
-print(scene_file.path)
-print(scene_file.filename)
+        Returns:
+            Path: The path to the scene file if successful
+        """
+        try:
+            return pmc.system.saveAs(self.path)
+        except RuntimeError as err:
+            log.warning("Missing Directories in path. Creating folders...")
+            self.folder_path.makedirs_p()
+            return pmc.system.saveAs(self.path)
